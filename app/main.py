4c49738a24fbd935d77d6c279b607a6fc1d60f64
Gauravsknows <gauravsknows@gmail.com>
2024-07-17 16:21:48 +0545

    Feat:changed the /index to /

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ app.add_middleware(
 #setup for Jinja2 templates
 templates = Jinja2Templates(directory="templates")
 
-@app.get('/index', response_class=HTMLResponse)
+@app.get('/', response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request":request})
 
